d86c76e0922ae4e8a84d79912e4dc78ec942c10a
tryHonestly <lordikus02021999@gmail.com>
2023-03-08 11:13:00 +0200

    final

--- src/loaders/CatalogCardLoader.tsx
+++ src/loaders/CatalogCardLoader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Skeleton } from '@mui/material'
 
 const CatalogCardLoader = React.memo(() => {
-  return <Skeleton variant="rectangular" width={'100%'} height={150} />
+  return <Skeleton variant="rectangular" width={'100%'} height={185} />
 })
 
 export default CatalogCardLoader
